b3c3127c991cd6e6c4b5aba5deb22c6a6dd034b7
Peter <pikepa@users.noreply.github.com>
2022-05-16 13:13:11 +0800

    Adding workbench psr-12 formatting changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-declare (strict_types=1);
+declare(strict_types=1);
 
 namespace App\Models;
 
@@ -12,7 +12,6 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-   
     use HasFactory;
     use Notifiable;
     use HasApiTokens;
