4a7ecc2669be059fc7ead49f04dbe30bae306094
Peter <pikepa@users.noreply.github.com>
2022-05-16 15:20:41 +0800

    adding uuid trait and giving to profile

---
+++ app/Models/Concerns/HasUuid.php
@@ -0,0 +1,20 @@
+<?php
+
+declare (strict_types=1);
+
+namespace App\Models\Concerns;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+trait HasUuid
+{
+    public static function bootHasUuid() :void  
+    {
+        static::creating(fn (Model $model)=>
+        
+        $model->uuid = Str::uuid()->toString(),
+    );
+    }
+
+}
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Concerns\HasUuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Profile extends Model
 {
         use HasFactory;
+        use HasUuid; 
 
     protected $fillable= [
         'uuid',
