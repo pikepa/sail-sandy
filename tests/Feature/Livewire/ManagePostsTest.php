9b6d34588d21a2440c90f7c91f04b6b5a71f3e90
Peter <pikepa@users.noreply.github.com>
2022-06-13 10:51:05 +0800

    update post test to reflect requirement for category id

--- tests/Feature/Livewire/ManagePostsTest.php
+++ tests/Feature/Livewire/ManagePostsTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Livewire\Livewire;
+use App\Models\Category;
 use App\Http\Livewire\Posts\ShowPost;
 use App\Http\Livewire\Posts\ManagePosts;
 
@@ -13,7 +14,9 @@ test('an authorised user sees the Manage Posts page', function () {
 });
 
 test('A guest can view a published post', function () {
-    $post = Post::factory()->create();
+    Category::factory()->create();
+
+    $post = Post::factory()->create(['category_id'=>1]);
 
    Livewire::test(ShowPost::class,['slug' => $post->slug])
        ->assertStatus(200)
@@ -26,9 +29,10 @@ test('A guest can view a published post', function () {
 test('An authorised user can see a list of all posts', function () {
     
     $this->signIn();
+    Category::factory()->create();
 
-    $post1 = Post::factory()->create();
-    $post2 = Post::factory()->create();
+    $post1 = Post::factory()->create(['category_id'=>1]);
+    $post2 = Post::factory()->create(['category_id'=>1]);
 
     $this->get('/posts/')
         ->assertSee($post1->title)
