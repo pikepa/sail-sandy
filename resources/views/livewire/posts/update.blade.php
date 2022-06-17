7e8e5b5a2462a5164444d3c4f74406c18741c7ed
Peter <pikepa@users.noreply.github.com>
2022-06-17 22:57:45 +0800

    added update.blade.php

---
+++ resources/views/livewire/posts/update.blade.php
@@ -0,0 +1,9 @@
+<div>
+        <x-forms.card title="Edit Post">
+            <x-forms.errors :errors="$errors"></x-forms.errors>
+
+            <form wire:submit.prevent="update({{ $this->post_id }})">
+                @include('livewire.posts.form', ['selectedCategory'=>$selectedCategory] )
+            </form>
+    </x-forms.card>
+</div>      
