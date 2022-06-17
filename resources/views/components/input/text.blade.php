be48fdada3eda27ac0067eae742ef842a3405d02
Peter <pikepa@users.noreply.github.com>
2022-06-17 21:36:42 +0800

    minor mods to font sizes

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -4,5 +4,5 @@
 
 <div >
     <input {{ $attributes }}
-        class=" p-2 border-1 border-gray-500 rounded-lg flex-1  block w-full ">
+        class=" p-2 border-1 border-gray-500 text-lg rounded-lg flex-1  block w-full ">
 </div>
--- resources/views/components/input/textarea.blade.php
+++ resources/views/components/input/textarea.blade.php
@@ -1,3 +1,3 @@
 <div class="flex ">
-    <textarea {{ $attributes }}  class="form-textarea rounded block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
+    <textarea {{ $attributes }}  class="text-lg form-textarea rounded block w-full transition duration-150 ease-in-out "></textarea>
 </div>
