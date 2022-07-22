a320e7dbcbdcdb0225a5021a8eb2d315caac26fa
Peter Pike <pikepeter@gmail.com>
2022-07-22 17:22:48 +0800

    modifications to clean up borders and move gallery to end of Photo/text

--- resources/views/livewire/posts/show-post.blade.php
+++ resources/views/livewire/posts/show-post.blade.php
@@ -12,33 +12,42 @@
 
                 </menus>
             </div>
-            <div class="py-2 bg-cyan-100" >
-                <div class="max-w-7xl mx-auto ">
-<div class="flex flex-row justify-between items-center">
-    <div class="ml-4 pt-2 text-3xl font-semibold">
-        {{$post->title}}
-       @isset($post->published_at) <p class="text-xs font-bold text-gray-600">Published on {{$post->published_at->toFormattedDateString()}} by {{$post->author->name}}</p>@endisset
-       @empty($post->published_at) <p class="text-xs font-bold text-gray-600">Not Published - Draft by {{$post->author->name}}</p>@endempty
+            <div class="py-2">
+                <div class="max-w-7xl mx-auto">
+                    <div class=" border-gray-900 border-2 rounded-lg">
+                        <div class="flex flex-row justify-between items-center">
+                            <div class="ml-4 pt-2  text-3xl font-semibold">
+                                {{$post->title}}
+                                @isset($post->published_at) <p class="text-xs font-bold text-gray-600">Published on
+                                    {{$post->published_at->toFormattedDateString()}} by {{$post->author->name}}</p>
+                                @endisset
+                                @empty($post->published_at) <p class="text-xs font-bold text-gray-600">Not Published -
+                                    Draft
+                                    by {{$post->author->name}}</p>@endempty
 
-    </div>
-    <div class="text-right mr-4 font-bold text-xl">
-        <a href="{{ url()->previous() }}"><i class="fa-solid fa-arrow-left"></i> Back</a>
-    </div>
-
-</div>
-                    <div class="bg-cyan-100 overflow-hidden shadow-sm rounded-lg">
-                        <div class="p-4  border-b border-gray-200">
-                            <div style="float:left;" class="mr-6">
-                                <img src="{{$post->cover_image}}" width='400px' alt="Cover Image">
                             </div>
-
-                            <div class="trix-content">
-                                    {!!$post->body!!}
+                            <div class="text-right mr-4 font-bold text-xl">
+                                <a href="{{ url()->previous() }}"><i class="fa-solid fa-arrow-left"></i> Back</a>
                             </div>
 
                         </div>
-                        <livewire:posts.display-post-gallery :post='$post'/>
-                        
+                        <div class=" overflow-hidden shadow-sm rounded-lg pb-4">
+                            <div class="p-4">
+                                <div style="float:left;" class="mr-6">
+                                    <img class="rounded-lg" src="{{$post->cover_image}}" width='400px'
+                                        alt="Cover Image">
+                                </div>
+
+                                <div class="trix-content">
+                                    {!!$post->body!!}
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <livewire:posts.display-post-gallery :post='$post' />
                     </div>
                 </div>
             </div>
