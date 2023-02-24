41844016f00e58cbcffb6356976ba1ea67394396
Peter Pike <pikepeter@gmail.com>
2023-02-24 15:19:58 +0800

    Extracted book column to component

---
+++ resources/views/components/pages/home_rhs_column.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <div class="text-center font-semibold text-3xl">Newsletter</div>
+    <div class="p-2  pb-4 rounded-lg border-2 border-gray-200">
+        <a href="/subscribers/create">
+            <button
+                class="rounded text-center text-xl font-bold text-teal-900  bg-cyan-400 p-2 mt-2">Subscribe
+                to our Newsletter</button>
+        </a>
+    </div>
+
+    <div class="rounded-lg border-2 border-gray-200 ">
+        <div class="p-2 text-center text-3xl Font-bold text-teal-900 pb-4">
+            The Book
+        </div>
+        <div class="row-span-2 row-end-auto">
+            <img class='rounded-lg' src="images/PT-Front-Cover.jpeg" alt="Book_cover">
+        </div>
+    </div>
+</div>
