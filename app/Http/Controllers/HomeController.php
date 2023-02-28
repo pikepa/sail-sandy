064b18deacc6838519c46bf13b43c9734f86a1c7
Peter Pike <pikepeter@gmail.com>
2023-02-28 15:41:44 +0800

    minor tweeks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,8 @@ class HomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $channels = Channel::where('status', true)->get();
+        $channels = Channel::where('status', true)
+        ->orderBy('sort', 'asc')->get();
         return view('home')->with(['channels' => $channels]);
     }
 }
