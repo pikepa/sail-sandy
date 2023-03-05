2614913d968e9495e5afb9be760c13b02f3c63cd
Peter Pike <pikepeter@gmail.com>
2023-03-05 15:49:43 +0800

    Minor formatting tweeks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
                         <div class="m-4 text-5xl text-center mt-12 p-8 border-b-2 text-gray-700 font-bold">
                             {{$post->title}}
                         </div>
-                        <div class=" overflow-hidden shadow-sm rounded-lg pb-4">
+                        <div class=" overflow-hidden shadow-sm min-h-full rounded-lg pb-4">
                             <div class="p-4">
                                 <div style="float:left;" class="mr-6">
                                     <img class="rounded-lg" src="{{$post->cover_image}}" width='400px'
