ab6664a68e2fa8b1dbc73b30e9081a4fd680a4e6
Peter Pike <pikepeter@gmail.com>
2023-03-08 18:39:30 +0800

    Minor formatting tweeks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,21 +17,19 @@
                                 </div>
 
                             </div>
-
-                            <div class="px-8 flex flex-row justify-between items-center">
-                                <div class=" ">
-                                    <a href="/subscribers/create">
-                                        <div
-                                            class="rounded text-center text-xl font-bold text-teal-900  bg-cyan-400 py-2 px-4">
-                                            Subscribe to our Newsletter
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="text-right font-bold text-xl">
-                                    <a href="/home"> Please Enter <i class="fa-solid fa-arrow-right"></i></a>
-                                </div>
+                        </div>
+                        <div class="px-8 flex flex-row justify-between items-center">
+                            <div class=" ">
+                                <a href="/subscribers/create">
+                                    <div
+                                        class="rounded text-center text-xl font-bold text-teal-900  bg-cyan-400 py-2 px-4">
+                                        Subscribe to our Newsletter
+                                    </div>
+                                </a>
                             </div>
-                            
+                            <div class="text-right font-bold text-xl">
+                                <a href="/home"> Please Enter <i class="fa-solid fa-arrow-right"></i></a>
+                            </div>
                         </div>
                 </div>
         </x-pages.standard-page>
