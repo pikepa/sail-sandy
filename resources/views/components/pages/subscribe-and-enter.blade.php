6176e1339d07d77139054668aa632a82bb61c4c4
Peter Pike <pikepeter@gmail.com>
2023-03-16 19:36:33 +0800

    modified welcome page and put the subscribe and enter to a component.pages.subscribe-and-enter

---
+++ resources/views/components/pages/subscribe-and-enter.blade.php
@@ -0,0 +1,13 @@
+<div class="px-8 flex flex-row justify-between items-center">
+    <div class=" ">
+        <a href="/subscribers/create">
+            <div
+                class="rounded-lg text-center text-xl font-bold text-teal-900  py-2 px-4">
+                Subscribe to our Newsletter
+            </div>
+        </a>
+    </div>
+    <div class="text-right font-bold text-xl">
+        <a href="/home"> Please Enter <i class="fa-solid fa-arrow-right"></i></a>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,13 @@
 <div>
     <x-app-layout>
         <x-pages.standard-page>
+
                 <div class="max-w-7xl min-h-screen mx-auto">
                         <div class=" text-5xl text-center p-8 border-b-2 text-gray-700 font-bold">
                             {{$post->title}}
                         </div>
+                        <x-pages.subscribe-and-enter />
+
                         <div class=" overflow-hidden  min-h-full rounded-lg pb-4">
                             <div class="p-4">
                                 <div style="float:left;" class="mr-6">
@@ -19,32 +22,20 @@
                             </div>
                         </div>
  
-                        <div class="px-8 flex flex-row justify-between items-center">
-                            <div class=" ">
-                                <a href="/subscribers/create">
-                                    <div
-                                        class="rounded-lg text-center text-xl font-bold text-teal-900  py-2 px-4">
-                                        Subscribe to our Newsletter
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="text-right font-bold text-xl">
-                                <a href="/home"> Please Enter <i class="fa-solid fa-arrow-right"></i></a>
-                            </div>
-                        </div>
                                                <!-- design details -->
-                                               <div class="grid grid-cols-4 border rounded-lg bg-slate-50 gap-4 mx-4 p-4 mt-8 text-sm mb-8">
-                                                <div>
-                                                    <div>Publisher: {{config('constants.publisher')}}</div>
-                                                    <div>Contact: {{config('constants.publisher_contact')}}</div>
-                                                </div>
-                                                <div>
+                                               <div class="flex flex-row justify-between border rounded-lg bg-slate-50 gap-4 mx-4 p-4 mt-8 text-sm mb-8">
+
+                                                <!-- <div>
                                                     <div>Publisher: {{config('constants.artworks')}}</div>
                                                     <div>Contact: {{config('constants.artworks_contact')}}</div>
-                                                </div>
+                                                </div> -->
                                                 <div>
                                                     <div>{{config('constants.design')}}</div>
                                                     <div>{{config('constants.design_by')}}</div>
+                                                </div>
+                                                <div>
+                                                    <div>Publisher: {{config('constants.publisher')}}</div>
+                                                    <div>Contact: {{config('constants.publisher_contact')}}</div>
                                                 </div>
                                                 <div>
                                                     <div>Website by: {{config('constants.website')}}</div>
