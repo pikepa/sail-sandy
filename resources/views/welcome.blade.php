5fa3452e130a18aaf15a37f61036e83c82a3a442
Peter Pike <pikepeter@gmail.com>
2023-03-17 14:50:50 +0800

    modified home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                         </div>
                         <x-pages.subscribe-and-enter />
 
-                        <div class=" overflow-hidden  min-h-full rounded-lg pb-4">
+                        <div class=" overflow-hidden  min-h-full rounded-lg ">
                             <div class="p-4">
                                 <div style="float:left;" class="mr-6">
                                     <img class="rounded-lg" src="{{$post->cover_image}}" width='400px'
@@ -21,27 +21,26 @@
 
                             </div>
                         </div>
- 
-                                               <!-- design details -->
-                                               <div class="flex flex-row justify-between border rounded-lg bg-slate-50 gap-4 mx-4 p-4 mt-8 text-sm mb-8">
+                            <!-- design details -->
+                            <div class="flex flex-row justify-between border rounded-lg bg-slate-50 gap-4 mx-4 p-4 mt-8 text-sm mb-8">
 
-                                                <!-- <div>
-                                                    <div>Publisher: {{config('constants.artworks')}}</div>
-                                                    <div>Contact: {{config('constants.artworks_contact')}}</div>
-                                                </div> -->
-                                                <div>
-                                                    <div>{{config('constants.design')}}</div>
-                                                    <div>{{config('constants.design_by')}}</div>
-                                                </div>
-                                                <div>
-                                                    <div>Publisher: {{config('constants.publisher')}}</div>
-                                                    <div>Contact: {{config('constants.publisher_contact')}}</div>
-                                                </div>
-                                                <div>
-                                                    <div>Website by: {{config('constants.website')}}</div>
-                                                    <div>Contact: {{config('constants.website_contact')}}</div>
-                                                </div>
-                                            </div>
+                            <!-- <div>
+                                <div>Publisher: {{config('constants.artworks')}}</div>
+                                <div>Contact: {{config('constants.artworks_contact')}}</div>
+                            </div> -->
+                            <div>
+                                <div>{{config('constants.design')}}</div>
+                                <div>{{config('constants.design_by')}}</div>
+                            </div>
+                            <div>
+                                <div>Publisher: {{config('constants.publisher')}}</div>
+                                <div>Contact: {{config('constants.publisher_contact')}}</div>
+                            </div>
+                            <div>
+                                <div>Website by: {{config('constants.website')}}</div>
+                                <div>Contact: {{config('constants.website_contact')}}</div>
+                            </div>
+                        </div>
                 </div>
         </x-pages.standard-page>
     </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
     ],
 };
