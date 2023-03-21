187a3abd292e3252a9b19f864e1528e52d9f1bc7
Peter Pike <pikepeter@gmail.com>
2023-03-21 17:14:20 +0800

    added links to left side of bottom menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,13 +23,8 @@
         <footer class="grid grid-cols-1 gap-2 bg-cyan-700 p-4 text-gray-100 rounded-b-lg">
             <div class="mt-2 grid grid-cols-3 gap-2 ">
                 <div class="p-4">
-                    <h5 class=" font-semibold border-b-4 border-gray-100">We're also on Social Media</h5>
-                    <ul class="list-disc p-2">
-                        <li><a href='https://twitter.com/lukeanthonyhunt' target="_blank" > Twitter</a></li>
-                        <li><a href='https://www.linkedin.com/in/luke-hunt-150b231/' target="_blank" > LinkedIn</a></li>
-                        <li><a href='https://www.pinterest.com.au/huntluke/' target="_blank" > Pinterest</a></li>
-
-                    </ul>
+                    <h5 class=" font-semibold border-b-4 border-gray-100">Social Media</h5>
+                        <livewire:home.display-links position="LEFT"/>
                 </div>
                 <div class="p-4">
                     <h5 class=" font-semibold border-b-4 border-gray-100">Press Clubs</h5>
