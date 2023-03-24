aa2db612987b6ae389472c28329202b2fa45c7b4
Peter Pike <pikepeter@gmail.com>
2023-03-24 21:18:20 +0800

    channel cards displaying ok

--- resources/views/components/posts/card.blade.php
+++ resources/views/components/posts/card.blade.php
@@ -1,12 +1,12 @@
-<div class="bg-cyan-100 rounded h-96 overflow-hidden shadow-lg">
+<div class="bg-cyan-100 rounded h-100 overflow-hidden shadow-lg">
     <a href="/posts/{{$post->slug}}">
         <div class="flex flex-col justify-between">
             <div class="">
-                <img class="object-cover h-52 w-full rounded-md " src='{{$post->cover_image}}' alt="placeimg">
+                <img class="h-60 w-full rounded-md object-cover object-center" src='{{$post->cover_image}}' alt="placeimg">
             </div>
-            <div class=" max-h-36 overflow-hidden">
+            <div class="h-40 overflow-hidden">
                 <div class="px-2 pt-2 font-bold text-base ">{{$post->title}}</div>
-                <div class="p-2  text-sm text-clip">
+                <div class="p-2 text-sm text-clip">
                     {!! $post->body!!}
                 </div>
             </div>
