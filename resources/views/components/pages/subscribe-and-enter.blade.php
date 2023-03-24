843befcd99c84c284126ebc6ac59e8d521f8dffe
Peter Pike <pikepeter@gmail.com>
2023-03-24 19:25:26 +0800

    formatting changes for welcome page for mobile and full screen

--- resources/views/components/pages/subscribe-and-enter.blade.php
+++ resources/views/components/pages/subscribe-and-enter.blade.php
@@ -1,13 +1,13 @@
-<div class="px-8 flex flex-row justify-between items-center">
+<div class="m-4 flex flex-col space-y-2 sm:flex-row justify-between items-center px-4">
     <div class=" ">
         <a href="/subscribers/create">
             <div
-                class="rounded-lg text-center text-xl font-bold text-teal-900  py-2 px-4">
-                Subscribe to our Updates.
+                class="rounded-lg text-xl font-bold text-teal-900">
+                Subscribe for Updates here.
             </div>
         </a>
     </div>
-    <div class="text-right font-bold text-xl">
-        <a href="/home"> Please Enter <i class="fa-solid fa-arrow-right"></i></a>
+    <div class="text-right font-bold text-xl  text-teal-900">
+        <a href="/home"> Click to Enter <i class="fa-solid fa-arrow-right"></i></a>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,31 +3,30 @@
         <x-pages.standard-page>
 
                 <div class="max-w-7xl min-h-screen mx-auto">
-                        <div class=" text-5xl text-center p-8 border-b-2 text-gray-700 font-bold">
+                        <div class=" text-5xl text-center p-8 rounded-lg border-b-2 text-gray-100 bg-teal-800 font-bold">
                             {{$post->title}}
                         </div>
-                        <x-pages.subscribe-and-enter />
+                        <div class="mx-auto">
+                            <x-pages.subscribe-and-enter />
+                        </div>
+                        <div class="min-h-full rounded-lg">
+                            <div class=" flex flex-col ">
+                                <div class="p-4  ">
+                                    <div  class="mx-auto sm:float-left sm:w-1/3 sm:mr-4 rounded-lg">
+                                        <img class="rounded-lg" src="{{$post->cover_image}}" 
+                                            alt="Cover Image">
+                                    </div>
 
-                        <div class=" overflow-hidden  min-h-full rounded-lg ">
-                            <div class="p-4">
-                                <div style="float:left;" class="mr-6">
-                                    <img class="rounded-lg" src="{{$post->cover_image}}" width='400px'
-                                        alt="Cover Image">
-                                </div>
-
-                                <div class="trix-content">
-                                    {!!$post->body!!}
+                                    <div class="mt-6 sm:mt-0  text-justify">
+                                        {!!$post->body!!}
+                                    </div>
                                 </div>
 
                             </div>
                         </div>
                             <!-- design details -->
-                            <div class="flex flex-row justify-between border rounded-lg bg-slate-50 gap-4 mx-4 p-4 mt-8 text-sm mb-8">
+                            <div class="flex flex-col sm:flex-row justify-between border rounded-lg bg-slate-50 gap-4 mx-4 p-4 mt-8 text-sm mb-8">
 
-                            <!-- <div>
-                                <div>Publisher: {{config('constants.artworks')}}</div>
-                                <div>Contact: {{config('constants.artworks_contact')}}</div>
-                            </div> -->
                             <div>
                                 <div>{{config('constants.design')}}</div>
                                 <div>{{config('constants.design_by')}}</div>
